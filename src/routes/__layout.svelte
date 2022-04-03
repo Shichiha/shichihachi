e5b06d514820a2a912e4824884f1ed1271a9eb22
Shichiha <77842398+78ThousandNumbers@users.noreply.github.com>
2022-04-03 16:15:29 +0800

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,62 @@
+<script>
+	import css from '../styles/global.css';
+</script>
+
+<main>
+	<header>
+		<p class="subtitle">Made with Svelte</p>
+		<h1 class="title">Shichihachi's page</h1>
+	</header>
+	<div class="content">
+		<slot />
+	</div>
+	<footer>
+		<p>manducare dolorem tuum sicut subcinericius.</p>
+	</footer>
+</main>
+
+<style>
+	footer {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		height: 5%;
+		background-color: #00000053;
+	}
+	header {
+		position: absolute;
+		top: 0;
+		width: 100%;
+		height: 50px;
+		background-color: #00000053;
+		padding: 0;
+		margin: 0;
+	}
+	p,
+	h1 {
+		padding-left: 10px;
+	}
+	.title,
+	.subtitle {
+		position: absolute;
+		right: 1%;
+	}
+	.subtitle {
+		color: #ffffff8d;
+	}
+	.content {
+		position: absolute;
+		top: 50px;
+		bottom: 50px;
+		left: 0;
+		right: 0;
+		display: flex;
+		flex-direction: column;
+        justify-content: center;
+        align-items: center;
+	}
+	p {
+		margin: 5px;
+		font-size: large;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+	console.log('Hello, you are currently viewing the index page');
+</script>
+
+<img src="https://cdn.discordapp.com/avatars/703461363573456928/dd838e3edbbfdafd0dc27398fb5e06fb.png?size=512" alt="Shichihachi" />
+<h1>wait fr?</h1>
+
+<style>
+	img {
+		animation: floating 2.8s infinite;
+	}
+	h1 {
+		animation: floating 2.8s infinite;
+	}
+	@keyframes floating {
+		0% {
+			transform: translateY(0);
+		}
+		50% {
+			transform: translateY(30px);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+
+</style>
