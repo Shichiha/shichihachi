03919f55ee7b3ff615bfef3bc44659bde6acad00
Nanako <00sh.chiha+github@gmail.com>
2022-08-20 20:32:48 +0900

    Another slight redesign
    more responsive sidebar
    localstorage w/ dark-mode
    minimalist-y look

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,15 @@
 <script>
 	import css from '../styles/global.scss';
 	let id = '703461363573456928';
+	
 </script>
 
-<div class="page">
-	<div class="small top">
-		<a href="https://discord.com/users/{id}">
-			<img
-				class="reveal"
-				src="https://lanyard.cnrad.dev/api/{id}?idleMessage=Not doing anything&bg=151515"
-				alt="Shichiha Discord"
-			/>
-		</a>
-		<a href="https://github.com/Shichiha">
-			<img
-				class="reveal"
-				src="https://github-readme-stats.vercel.app/api?username=Shichiha&count_private=true&show_icons=true&theme=radical&title_color=fff&icon_color=79ff97&text_color=9f9f9f&bg_color=151515"
-				alt="Shichiha Stats"
-			/>
-		</a>
-	</div>
-	<p class="center">Hi, I'm Shichiha. a programmer. (yes, that is all)</p>
+<div class='content'>
+	<h1>Hello, World</h1>
+	<p>I am Shichiha! Nice to meet you.</p>
+	<p>You can reach me through the platforms below 😉</p>
+	<div >
+		<a href="https://discord.com/users/{id}"><img src="https://discord-readme-badge.vercel.app/api?id={id}" alt="Shichiha Discord" /></a>
+		<a href="https://github.com/Shichiha"><img src="https://github-readme-stats.vercel.app/api?username=Shichiha&count_private=true" alt="Shichiha Stats"/></a>
+	</div>	
 </div>
--- src/routes/more.svelte
+++
@@ -1,3 +0,0 @@
-<div class="center">
-	<p>contact me on discord and tell me what more to do in my website</p>
-</div>
